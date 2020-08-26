4cab58369b11cf9f60be4c52fc237dcd78e58456
Sebastian Stefaniuk <sstefaniuk@miteksystems.com>
2020-08-26 16:49:22 -0700

    Couple more updates to mds manager page

--- scripts/main.js
+++ scripts/main.js
@@ -1,14 +1,35 @@
 $(document).ready(function() {
 
+    var util = {
+        loadMdsIds: function() {
+
+        }
+    };
+
     // Landing page
 
     // MDS Manager page
     $('#mds-table').DataTable({
         columns: [
-            { searchable: true },
-            { searchable: true },
+            { 
+                searchable: false, 
+                sortable: false, 
+                width: "40px" 
+            },
+            { 
+                searchable: true 
+            },
+            { 
+                searchable: false, 
+                width: "180px" 
+            },
+            { 
+                searchable: true, 
+                width: "120px" 
+            },
         ],
         paging: false,
         searching: false,
     });
+
 } );
